a4dbf471797e938400501e197e2b7333f26468e0
MarinRatkovicWork <86005829+MarinRatkovicWork@users.noreply.github.com>
2022-12-18 21:34:33 +0100

    Build *(Set up fore android build)*

--- Novena/Assets/Scripts/GameManager.cs
+++ Novena/Assets/Scripts/GameManager.cs
@@ -1,12 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.UI;
-using static UnityEditor.Experimental.AssetDatabaseExperimental.AssetDatabaseCounters;
 
 public class GameManager : MonoBehaviour
 {
